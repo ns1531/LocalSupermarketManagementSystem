2ec6bfe55821201f8f26ac4bcc708a7827b75539
Navneet Sookraz <ns1531@live.mdx.ac.uk>
2026-06-21 20:15:43 +0400

    Added initial product catalogue array structure

---
+++ SupermarketManagementSystem/ShopDataStructures/ProductCatalogueArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SupermarketManagementSystem.ShopData;
+
+namespace SupermarketManagementSystem.ShopDataStructures
+{
+    public class ProductCatalogueArray
+    {
+        private Product[] products;
+        private int count;
+
+        public ProductCatalogueArray()
+        {
+            products = new Product[5];
+            count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (count == products.Length)
+            {
+                ResizeArray();
+            }
+
+            products[count] = product;
+            count++;
+        }
+
+        public Product? GetProductAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                return null;
+            }
+
+            return products[index];
+        }
+
+        private void ResizeArray()
+        {
+            Product[] largerArray = new Product[products.Length * 2];
+
+            for (int i = 0; i < count; i++)
+            {
+                largerArray[i] = products[i];
+            }
+
+            products = largerArray;
+        }
+    }
+}

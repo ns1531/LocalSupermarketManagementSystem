975fe99e28f0a8c60a43e6ce1dbb1644919db1f7
Navneet Sookraz <ns1531@live.mdx.ac.uk>
2026-06-26 13:29:25 +0400

    Added sale transaction and sale item classes

---
+++ SupermarketManagementSystem/ShopData/Sale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupermarketManagementSystem.ShopData
+{
+    public class Sale
+    {
+        public string SaleId { get; set; } = "";
+        public DateTime SaleDate { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ SupermarketManagementSystem/ShopData/SaleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupermarketManagementSystem.ShopData
+{
+    public class SaleItem
+    {
+        public string SaleId { get; set; } = "";
+        public string ProductId { get; set; } = "";
+        public string ProductTitle { get; set; } = "";
+        public int QuantitySold { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal ItemTotal { get; set; }
+    }
+}

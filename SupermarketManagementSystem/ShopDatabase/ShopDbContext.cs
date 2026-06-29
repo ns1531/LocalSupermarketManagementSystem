ec1fa4876a9dc667a99b4b3e9b45d0c0811e6a9d
Navneet Sookraz <ns1531@live.mdx.ac.uk>
2026-06-29 15:27:01 +0400

    Added shop database context using Entity Framework

---
+++ SupermarketManagementSystem/ShopDatabase/ShopDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using SupermarketManagementSystem.ShopData;
+
+namespace SupermarketManagementSystem.ShopDatabase
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Supplier> Suppliers { get; set; } = null!;
+        public DbSet<Sale> Sales { get; set; } = null!;
+        public DbSet<SaleItem> SaleItems { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=LocalSupermarketManagementSystem;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}

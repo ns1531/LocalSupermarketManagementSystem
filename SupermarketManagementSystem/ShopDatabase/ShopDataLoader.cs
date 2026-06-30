ca826c45b95119680247b240dc1cf0f26e3f2388
Navneet Sookraz <ns1531@live.mdx.ac.uk>
2026-06-30 11:58:34 +0400

    Added database loader for products and suppliers

---
+++ SupermarketManagementSystem/ShopDatabase/ShopDataLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using SupermarketManagementSystem.ShopData;
+using SupermarketManagementSystem.ShopDataStructures;
+
+namespace SupermarketManagementSystem.ShopDatabase
+{
+    public class ShopDataLoader
+    {
+        public static ProductCatalogueArray LoadProducts()
+        {
+            ProductCatalogueArray productCatalogue = new ProductCatalogueArray();
+
+            using ShopDbContext database = new ShopDbContext();
+
+            foreach (Product product in database.Products)
+            {
+                Stock? stock = database.Stock.Find(product.ProductId);
+
+                if (stock != null)
+                {
+                    product.QuantityInStock = stock.QuantityInStock;
+                    product.LowStockThreshold = stock.LowStockThreshold;
+                }
+
+                productCatalogue.AddProduct(product);
+            }
+
+            return productCatalogue;
+        }
+
+        public static SupplierRecordArray LoadSuppliers()
+        {
+            SupplierRecordArray supplierRecords = new SupplierRecordArray();
+
+            using ShopDbContext database = new ShopDbContext();
+
+            foreach (Supplier supplier in database.Suppliers)
+            {
+                supplierRecords.AddSupplier(supplier);
+            }
+
+            return supplierRecords;
+        }
+    }
+}
